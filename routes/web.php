73667ae8ab98f03f23f147440c8615d135011e67
Deepak Visani <deepakvisani@gmail.com>
2020-07-16 19:25:26 +0530

    first-commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use \Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/create-role/{name}', function (Request $request){
+    $role = Role::create(['name' => $request->name]);
+    dd($role);
+});
+
+Route::get('/assign-role/{name}', function (Request $request){
+    return \Illuminate\Support\Facades\Auth::user()->assignRole($request->name);
+});
